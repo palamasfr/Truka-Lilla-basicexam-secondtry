2494e9481a6086705acd4895b4f3335b267628dc
Lilla Truka <trukacsa@gmail.com>
2018-07-26 14:10:33 +0200

    js3

--- js/got.js
+++ js/got.js
@@ -1,19 +1,22 @@
 function getData(url, callbackFunc) {
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
+  xhttp.onreadystatechange = function firstFunction() {
+    if (this.readyState === 4 && this.status === 200) {
       callbackFunc(this);
     }
   };
-  xhttp.open("GET", url, true);
+  xhttp.open('GET', url, true);
   xhttp.send();
 }
 
 function successAjax(xhttp) {
   // itt a json content, benne a data változóban
-  var userDatas = JSON.parse(xhttp.responseText);
-  var realDatas = userDatas[2].data;
+  var realDatas = JSON.parse(xhttp.responseText);
   selectCharacters(realDatas);
+  sortedCharacters(realDatas);
+  showCharacters(realDatas);
+  addSearching(realDatas);
+  showDatasToUp();
   console.log(realDatas);
   /*
     Pár sorral lejebb majd ezt olvashatod:
@@ -40,19 +43,104 @@ function selectCharacters(parameterArr) {
     }
   }
 }
+function sortedCharacters(parameterArr) {
+  for (var i = 0; i < parameterArr.length - 1; i++) {
+    for (var j = i + 1; j < parameterArr.length; j++) {
+      var compHouses = parameterArr[i].house.localeCompare(parameterArr[j].house);
+      if (compHouses > 0) {
+        [parameterArr[i], parameterArr[j]] = [parameterArr[j], parameterArr[i]];
+      } else if (compHouses === 0) {
+        var compName = parameterArr[i].name.localeCompare(parameterArr[j].name);
+        if (compName > 0) {
+          [parameterArr[i], parameterArr[j]] = [parameterArr[j], parameterArr[i]];
+        }
+      }
+    }
+  }
+}
+
 function showCharacters(parameterArr) {
-  for (var i = 0; i < ParameterArr.length; i++) {
+  for (var i = 0; i < parameterArr.length; i++) {
     var characterDiv = document.createElement('div');
+    var main = document.querySelector('.down');
     characterDiv.className = 'oneCharacter';
     var characterImage = document.createElement('img');
-    var characterName = document.createElement('p');
-    var main = document.querySelector('.left');
+    characterImage.characterData = parameterArr[i];
+    characterImage.className = 'big-image';
+    characterImage.addEventListener('hover', function ev() {
+      this.src = `/assets/${parameterArr.portrait}`;
+      this.show;
+    });
+    var characterName = document.createElement('div');
+    characterName.className = 'character-name';
     var icon = document.createElement('img');
-    icon.src = `/assets/houses/${ParameterObj.house}.png`;
-    characterImage.src = `/${ParameterArr[i].portrait}`;
-    characterName.innerHTML = ParameterArr[i].name;
+    icon.addEventListener('error', function ev() {
+      this.src = '/assets/houses/errorIcon.png';
+    });
+    var houseName = document.createElement('div');
+    houseName.className = 'house-name';
+    var bio = document.createElement('div');
+    bio.className = 'bio';
+    icon.src = `/assets/houses/${parameterArr[i].house}.png`;
+    characterImage.src = `/${parameterArr[i].picture}`;
+    characterName.innerHTML = parameterArr[i].name;
+    houseName.innerHTML = parameterArr[i].house;
+    bio.innerHTML = parameterArr[i].bio;
     characterDiv.appendChild(characterImage);
     characterDiv.appendChild(characterName);
+    characterDiv.appendChild(icon);
+    characterDiv.appendChild(houseName);
+    characterDiv.appendChild(bio);
     main.appendChild(characterDiv);
   }
+}
+
+function addSearching(realDatas) {
+  var button = document.querySelector('#search-button');
+  var input = document.querySelector('#search-text');
+  input.placeholder = 'Search for characters';
+  button.addEventListener('click', function () {
+    searching(input.value, realDatas);
+    input.value = '';
+  });
+}
+
+function searching(text, realDatas) {
+  var found = false;
+  var i = 0;
+  while (!found && i < realDatas.length) {
+    if (realDatas[i].name.toLowerCase().indexOf(text.toLowerCase()) > -1) {
+      found = true;
+      showDatasToUp(realDatas[i]);
+      return;
+    }
+    i++;
+  }
+  if (!found) {
+    document.querySelector('.up').innerHTML = 'Character not found.';
+  }
+}
+
+function showDatasToUp(ParameterObj) {
+  var details = document.querySelector('.character-details');
+  details.innerHTML = '';
+  var nameOfCharacter = document.createElement('p');
+  nameOfCharacter.className = 'up-name';
+  var houseOfCharacter = document.createElement('p');
+  houseOfCharacter.className = 'up-house';
+  if (ParameterObj !== '') {
+    var icon = document.createElement('img');
+    icon.src = `/assets/houses/${ParameterObj.house}`;
+    details.appendChild(icon);
+  } else {
+    icon.src = '/assets/houses/errorIcon.png';
+  }
+  nameOfCharacter.innerHTML = ParameterObj.name;
+  houseOfCharacter.innerHTML = ParameterObj.house;
+  details.appendChild(nameOfCharacter);
+  details.appendChild(houseOfCharacter);
+  details.appendChild(icon);
+}
+function mouseHover() {
+
 }
